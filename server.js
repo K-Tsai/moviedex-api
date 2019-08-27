0500a862a97bae627b4a4b5139c97357dc1a4f63
Kevin Tsai <k.tsai92@gmail.com>
2019-08-27 15:02:34 -0400

    added server code

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const express = require('express')
 const morgan = require('morgan')
 let movieSet = require('./Movie-Dataset')
 
-console.log(process.env.API_TOKEN)
-
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 
 function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
@@ -52,7 +51,17 @@ app.get('/movie', validateBearerToken, (req, res) => {
     res.json(avgFilter)
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)

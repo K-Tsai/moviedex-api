135fbebb3b84b2084bc413793a078e7ab75607ff
Kevin Tsai <k.tsai92@gmail.com>
2019-08-26 22:58:51 -0400

    third commit

--- server.js
+++ server.js
@@ -9,33 +9,48 @@ const app = express()
 
 app.use(morgan('dev'))
 
-
-const validTypes = [`Animation`, `Drama`, `Romantic`, `Comedy`, `Spy`, `Crime`, `Thriller`, `Adventure`, `Documentary`, `Horror`, `Action`, `Western`, `History`, `Biography`, `Musical`, `Fantasy`, `War`, `Grotesque`]
-
-//app.use(function validateBearerToken(req, res, next) {
-   // console.log('validate bearer token middleware')
-   // next()
-//})
+function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next()
+}
 
 function handleGetGenre(req, res) {
-    let userGenre = req.query.genre
-    return movieSet.filter(movie => movie.genre == userGenre);
+		let userGenre = req.query.genre
+		if(userGenre) {
+			return movieSet.filter(movie => movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()));
+		} else {
+			return movieSet
+		}
 }
 
 function handleGetCountry( req, res, moviefilter) {
     let userCountry = req.query.country
-    return moviefilter.filter(movie => movie.country == userCountry)
+    if (userCountry) {
+        return moviefilter.filter(movie => movie.country.toLowerCase().includes(req.query.country.toLowerCase()))
+    } else { 
+        return moviefilter
+    }
 }
 
+function handleGetAverage(req, res, avgfilter) {
+	let userAvg= req.query.avg_vote
+	if (userAvg) {
+		return avgfilter.filter(avg => avg.avg_vote >= Number(userAvg))
+	} else {
+		return avgfilter
+	}
+}
 
-
-app.get('/movie', (req, res) => {
+app.get('/movie', validateBearerToken, (req, res) => {
     let movieFilter = handleGetGenre(req, res)
-    let countryMovieFilter= handleGetCountry(req, res, movieFilter)
-    res.json(countryMovieFilter)
+		let countryMovieFilter= handleGetCountry(req, res, movieFilter)
+		let avgFilter= handleGetAverage(req, res, countryMovieFilter)
+    res.json(avgFilter)
 })
-
-
 
 const PORT = 8000
 
